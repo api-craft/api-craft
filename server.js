cab7561f80d16e535027da345d89c3efe7a54cb8
P.Thamilselven <contact@tselven.com>
2025-06-15 11:00:56 +0530

    add connect to mongodb code

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on http://localhost:${PORT}`);
 });
