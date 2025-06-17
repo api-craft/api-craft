c486805f9032199293f536fac6bd2cf874ab470c
P.Thamilselven <contact@tselven.com>
2025-06-17 15:06:41 +0530

    add auth endpoints

--- routes.js
+++ routes.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
 import { addUser,updateUser,deleteUser,getAllUsers,getUser } from './app/controllers/user.controller.js';
+import { register, login, profile, logout } from './app/controllers/auth.controller.js';
+import { requireAuth } from './app/middlewares/auth.middleware.js';
+
 const router = Router();
 
 router.get('/',(req,res) =>{
@@ -10,6 +13,10 @@ router.get('/users',getAllUsers);
 router.get('/users/:id',getUser);
 router.post('/users',addUser);
 router.put('/users/:id',updateUser);
-router.delete('/users',deleteUser);
+router.delete('/users/:id',deleteUser);
+router.post('/register', register);
+router.post('/login', login);
+router.get('/profile', requireAuth, profile);
+router.post('/logout', requireAuth, logout);
 
 export default router;
--- server.js
+++ server.js
@@ -2,16 +2,17 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import apiRoutes from './routes.js';
+import { corsMiddleware } from './config/cors.js';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
+app.use(corsMiddleware)
+
 app.use("/api",apiRoutes);
 const PORT = process.env.PORT || 3000;
-
-
 
 app.listen(PORT, () => {
   connectDB();
